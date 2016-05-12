85203808a2ca114945141727f5205637c0f0f21f
vamonte <valentin.monte@yahoo.fr>
2016-05-12 13:31:30 +0200

    Add a setup file to install the lib with pip

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='counter-decorator',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='Apache License 2.0',
+    description='Decorator to implement easily the woosmap counter behavior',
+    long_description="",
+    url='https://github.com/webgeoservices/counter_decorator',
+    author='webgeoservices',
+    author_email=',',
+    install_requires=[
+        'requests==2.9.1',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
+
