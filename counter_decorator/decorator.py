bf0bb012b7fb87ad9754251a48d06adb21e8b706
vamonte <valentin.monte@yahoo.fr>
2016-10-17 17:02:32 +0200

    The key is now the result of a lambda

--- counter_decorator/decorator.py
+++ counter_decorator/decorator.py
@@ -3,31 +3,16 @@ import os
 from functools import wraps
 
 
-def count_request(request_name, project_key_parent=None, project_key_name="project_key"):
+def project_key_lambda(*args, **kwargs):
+    return kwargs.get("project_key")
+
+def count_request(request_name, project_key_lambda=project_key_lambda):
     def wrapped(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
             product = os.environ.get("PRODUCT_NAME")
             host = os.environ.get("COUNTER_HOST")
-            if project_key_parent is None:
-                key = kwargs.get(project_key_name)
-            else:
-                _parents = project_key_parent.split(".")
-                try:
-                    parent = kwargs[_parents.pop(0)]
-                except KeyError:
-                    key = None
-                else:
-                    for _p in _parents:
-                        try:
-                            parent = getattr(parent, _p)
-                        except AttributeError:
-                            break
-                    try:
-                        key = parent.get(project_key_name)
-                    except AttributeError:
-                        key = None
-
+            key = project_key_lambda(*args, **kwargs)
             if host is not None and product is not None and key is not None:
                 counter_resource = "http://{host}/projects/{key}/products/{product}/kinds/{kind}".format(
                     host=host,
