a55aa37a8c2aeaa97d972a5a1ca4892162697c07
qelanhari <qelanhari@webgeoservices.com>
2017-02-24 11:41:27 +0100

    Take products as parameter instead of env var

--- counter_decorator/decorator.py
+++ counter_decorator/decorator.py
@@ -5,13 +5,13 @@ from .cua import build_job_data
 from .utils import (organization_from_token, project_key_lambda as default_project_key_lambda)
 
 
-def count_request(request_name, project_key_lambda=None, headers_lambda=None, name_lambda=None):
+def count_request(request_name, product=None, project_key_lambda=None, headers_lambda=None, name_lambda=None):
     project_key_lambda = project_key_lambda if project_key_lambda else default_project_key_lambda
+    product = product if product else os.environ.get("PRODUCT_NAME")
 
     def wrapped(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
-            product = os.environ.get("PRODUCT_NAME")
             key, kind_key = project_key_lambda(*args, **kwargs)
             kind = name_lambda(product, key, kind_key, *args, **kwargs) if name_lambda else request_name
             organization = organization_from_token(kwargs['readable_token'])
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='counter-decorator',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     license='Apache License 2.0',
