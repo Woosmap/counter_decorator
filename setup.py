e06fae73b7905883627ce71c3f2704f612076f64
Emmanuel Peralta <eperalta@webgeoservices.com>
2021-07-22 14:05:54 +0200

    Bumps counter decorator version to v0.2.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='counter-decorator',
-    version='0.1.6',
+    version='0.2.0',
     packages=find_packages(),
     include_package_data=True,
     license='Apache License 2.0',
